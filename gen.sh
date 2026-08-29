9fa6b1537b46e31117063146b373664c46158779
Joshua Bergman Andersson <38278628+Matistjati@users.noreply.github.com>
2026-08-29 16:58:47 +0200

    Make wait more robust to crashing generators (#42)

--- gen.sh
+++ gen.sh
@@ -496,8 +496,18 @@ grader_flags: first_error" > sample/testdata.yaml
 }
 _setup_dirs
 
+# Workaround for a bash bug where a normal `wait` can loop forever if the job list gets desynced
+# Bug report: https://www.mail-archive.com/bug-bash@gnu.org/msg35667.html
+# Fixed in bash 5.3.10 https://ftp.gnu.org/gnu/bash/bash-5.3-patches/bash53-010
+_wait_all_jobs () {
+  local pid
+  for pid in $(jobs -pr); do
+    wait "$pid" 2>/dev/null || true
+  done
+}
+
 _cleanup_programs () {
-  wait
+  _wait_all_jobs
   for x in "${cleanup[@]}"; do
     rm -f "$x"
   done
